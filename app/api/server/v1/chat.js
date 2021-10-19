d251f8471ad703932eeb95e3f0e3eac5a4e35b66
odejinmisa <odejinmisa@newwavesecosystem.com>
2021-10-19 11:14:08 +0100

    integrating with konn3ct room

--- app/api/server/v1/chat.js
+++ app/api/server/v1/chat.js
@@ -744,8 +744,7 @@ API.v1.addRoute('chat.rooms', {
 				status = true;
 				data = body.data;
 				msg = body.message;
-				return API.v1.success(body);
-
+				return API.v1.success({ body });
 			}
 			msg = body.message;
 			return API.v1.failure(msg);
@@ -774,7 +773,7 @@ API.v1.addRoute('chat.room.start', {
 					status = true;
 					url = body.url;
 					msg = body.message;
-					return API.v1.success(body);
+					return API.v1.success({ body });
 				}
 				msg = body.message;
 				return API.v1.failure(msg);
